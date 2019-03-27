23176a392fdce081f07dd4c6e399d585a3ea91ee
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-27 15:53:20 -0400

    add async await to handleAddNote

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ class App extends Component {
   };
   handleChangeNote = event => this.setState({ note: event.target.value });
 
-  handleAddNote = event => {
-    const { note } = this.state;
+  handleAddNote = async event => {
+    const { note, notes } = this.state;
     event.preventDefault();
     const input = {
       note
     };
-    API.graphql(graphqlOperation(createNote, { input }));
+    const result = await API.graphql(graphqlOperation(createNote, { input }));
+    const newNote = result.data.createNote;
+    const updatedNotes = [newNote, ...notes];
+    this.setState({ notes: updatedNotes, note: "" });
   };
   render() {
-    const { notes } = this.state;
+    const { notes, note } = this.state;
     return (
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-1">Amplify Notetaker</h1>
@@ -30,6 +33,7 @@ class App extends Component {
             className="pa2 f4"
             placeholder="Write your note"
             onChange={this.handleChangeNote}
+            value={note}
           />
           <button className="pa2 f4" type="submit">
             Add Note
