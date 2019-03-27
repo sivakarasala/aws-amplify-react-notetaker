4393602db9e879b2fc4f0fe44b777e6b8e2f3a3c
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-27 15:23:03 -0400

    Add authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withAuthenticator } from "aws-amplify-react";
 
 class App extends Component {
   render() {
@@ -6,4 +7,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
